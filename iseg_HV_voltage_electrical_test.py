7aabb9a1585890c6a4bbba45d5cfa1b1497400cf
meowzers003 <amshisivara03@gmail.com>
2026-07-24 16:59:23 -0400

    Add ISEG HV electrical test outline

---
+++ iseg_HV_voltage_electrical_test.py
@@ -0,0 +1,19 @@
+
+
+"""
+For electrical test for each channel, please develop script to perform:
+
+ 
+
+IV test, set voltage to 50V, 100V, 200V, 500V, 1000V, 1500V, 2000V, measure currents.
+Ramp rate test (up/down):
+ (a) ramp rate set to 10V/s, ramp up to 1000V and then down, make plots to get the ramp rate
+
+(b) ramp rate set to 100V, ramp up to 1000V and then down, make plots to get the ramp rate
+
+
+
+Refer to screenshots in recent draft and photos to know the testing scheme 
+
+"""
+
